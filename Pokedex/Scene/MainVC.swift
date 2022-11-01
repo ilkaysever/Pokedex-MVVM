57f609b2759c7b757d0a3deb9d85003619c3d05c
ilkay sever <ilkay.sever@applogist.com>
2022-11-01 16:46:51 +0300

    pokedex project created & pod file added

---
+++ Pokedex/Scene/MainVC.swift
@@ -0,0 +1,17 @@
+//
+//  MainVC.swift
+//  Pokedex
+//
+//  Created by İlkay Sever on 1.11.2022.
+//
+
+import UIKit
+
+class MainVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+}
